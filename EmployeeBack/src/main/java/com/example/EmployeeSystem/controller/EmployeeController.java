c41d4a56bd4fb767e3b0c7ceed7ecd40f32a6f4f
jrox <pedro.fernandes1058@gmail.com>
2024-03-02 23:52:28 -0300

    criando o controller

---
+++ EmployeeBack/src/main/java/com/example/EmployeeSystem/controller/EmployeeController.java
@@ -0,0 +1,11 @@
+package com.example.EmployeeSystem.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/employees")
+public class EmployeeController {
+}
