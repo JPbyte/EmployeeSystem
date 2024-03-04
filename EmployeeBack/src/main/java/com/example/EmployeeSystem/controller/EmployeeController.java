4b03c8313dc1f12601846bc4f2d69571babab002
jrox <pedro.fernandes1058@gmail.com>
2024-03-03 21:36:07 -0300

    Employee controller criado

--- EmployeeBack/src/main/java/com/example/EmployeeSystem/controller/EmployeeController.java
+++ EmployeeBack/src/main/java/com/example/EmployeeSystem/controller/EmployeeController.java
@@ -1,6 +1,12 @@
 package com.example.EmployeeSystem.controller;
 
+import com.example.EmployeeSystem.dto.EmployeeDTO;
+import com.example.EmployeeSystem.service.EmployeeService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 @RequestMapping("/api/employees")
 public class EmployeeController {
+
+    private EmployeeService empService;
+
+    @PostMapping
+    public ResponseEntity<EmployeeDTO> createEmployee(@RequestBody EmployeeDTO empDTO){
+        EmployeeDTO savedEmp = empService.createEmployee(empDTO);
+        return new ResponseEntity<>(savedEmp, HttpStatus.CREATED);
+    }
 }
